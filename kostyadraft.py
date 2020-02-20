393c4fd71aaa80df7311bdbf320bac3f2ab7be07
Konstantin Smirnov <windsurf111@yandex.ru>
2020-02-20 15:19:56 +0100

    slightly improved

--- kostyadraft.py
+++ kostyadraft.py
@@ -1,13 +1,15 @@
+print('max slices ' , goal)
 inp = [2,5,6,8]
 goal = 17
 
 def exists_higher(test_list, k):
     
+    test_list.sort()
     for i in test_list : 
         if i > k:
             return i
     
-    return 10000000000000000000
+    return -1
 
 def get_lower(test_list, k):
     
@@ -15,14 +17,18 @@ def get_lower(test_list, k):
     
     for i in test_list : 
         if i < k:
+            test_list.sort()
             return i
+        
     
-    return -1000000000000000000
+    test_list.sort()
+    return -1
 
 #keep track to have the diff value smallest absolute value : either negative or equal to zero
-def cancelout(curlist, diff):
+def cancelout(curlist, diff, goal):
     
     curlist.sort()
+    print('it! ')
     
     #if the exact number exists in the list - remove it and return - rare case
     if diff in inp:
@@ -31,29 +37,52 @@ def cancelout(curlist, diff):
         return curlist, diff
     
     #if there is an element higher value than current difference:     
-    elif exists_higher(curlist, diff) - diff < diff - get_lower(curlist, diff):
+    elif abs(exists_higher(curlist, diff) - diff) < abs(get_lower(curlist, diff) - diff) and exists_higher(curlist, diff) != -1 and get_lower(curlist, diff) != -1:
         
+#         print('higher than ', diff , ' is ' , exists_higher(curlist, diff))
+#         print('lower than ', diff , ' is ' , get_lower(curlist, diff))
+        
+        print('c ', curlist)
         toremove = exists_higher(curlist, diff)
         curlist.remove( toremove )
         diff = diff - toremove
         
         return curlist, diff
     
-    else:
+    #edge of the list...
+    elif get_lower(curlist, diff) == -1 and exists_higher(curlist, diff) != -1:
         
+        print('c2 ', curlist)
+        toremove = exists_higher(curlist, diff)
+        print('torem ', toremove)
+        curlist.remove( toremove )
+        print('c22 ' , curlist)
+        diff = diff - toremove
+        
+        return curlist, diff
+    
+    else:
+        print('c3 ' , curlist)
         lwr = get_lower(curlist, diff)
-        currlist.remove( lwr )
+        print('lwr ', lwr)
+        curlist.remove( lwr )
         diff = diff - lwr
         
-        if diff > 0 and not curlist.empty():
-            cancelout(currlist, diff)
+        print('lst2 ' , curlist, ' diff ', diff)
+        
+        
+        if diff > 0 and sum(curlist) > goal:
+            cancelout(curlist, diff, goal)
         
 
-def method1(inp, goal):
+def method1(inp, goal, goal2):
     sumlist = sum(inp)
     diffsum = sumlist - goal
     
-    print( cancelout(inp, diffsum))
+    if diffsum == 0:
+        return inp
+    else:
+        print( cancelout(inp, diffsum, goal2))
 
-method1(inp, goal)   
-    
+
+method1(inp, goal, goal2)
